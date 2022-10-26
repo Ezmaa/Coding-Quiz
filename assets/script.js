630b56ad654984a63bf1f924260115ced815c0a8
ezmaa <ezinmark@gmail.com>
2022-10-26 17:27:58 -0400

    added start button function that sets time when clicked on,  added function for each block of quesions that will show questions and answers both in js, added a div for main questions as well as h3, p, and start button to html,

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,88 @@
+// Add class selectors
 const timeEl = document.querySelector("h1");
+const questionEl = document.querySelector('h4')
+const questionOneEl = document.getElementById("q1");
+const questionTwoEl = document.getElementById("q2");
+const questionThreeEl = document.getElementById("q3");
+const questionFourEl = document.getElementById("q4");
+const highScoreEl = document.getElementById('high');
+const wrongOrRight = document.getElementById('wrong-right');
+const startButtonEl = document.getElementById('start-button');
 
+const start = true;
 let secondsLeft = 75;
 
-function setTime() {
+function init() {
+  getscores ();
+}
+// added start button 
+function beginQuiz() {
+  secondsLeft = 75;
+  startTime();
+}
+
+function questionOne() {
+  questionEl.textContent = "Which of the following keywords is used to define a variable in Javascript?";
+  const rightAnswer = questionOneEl;
+  questionOneEl.textContent = "Var";
+  questionTwoEl.textContent = "Let";
+  questionThreeEl.textContent = "Both 1 and 2";
+  questionFourEl.textContent = "None";
+  if (rightAnswer) {
+    wrongOrRight.textContent = "Correct"
+  } else {
+    wrongOrRight.textContent = "false"
+  }
+}
+
+function questionTwo() {
+  questionEl.textContent = "Javascript is an _______ language?";
+  questionOneEl.textContent = "Object-Oriented";
+  questionTwoEl.textContent = "Object-Based";
+  questionThreeEl.textContent = "Procedural";
+  questionFourEl.textContent = "None of the above";
+  if ()
+} 
+
+function questionThree() {
+  questionEl.textContent = "Which of the following methods is used to access HTML elements using Javascript?";
+  questionOneEl.textContent = "getElemntById()";
+  questionTwoEl.textContent = "getElementByClassName()";
+  questionThreeEl.textContent = "Both 1 and 2";
+  questionFourEl.textContent = "None of the above";
+  if ()
+}
+
+function questionFour() {
+  questionEl.textContent = "How can a datatype be declared to be a constant type?";
+  questionOneEl.textContent = "Const";
+  questionTwoEl.textContent = "Var";
+  questionThreeEl.textContent = "Let";
+  questionFourEl.textContent = "Constant";
+  if ()
+}
+
+startButtonEl.addEventListener('click', beginQuiz); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Create count down timer for quiz 
+
+
+function startTime() {
   const timerInterval = setInterval(function() {
     secondsLeft--;
     timeEl.textContent = "Time Left:" + secondsLeft;
-
     if(secondsLeft === 0) {
         clearInterval(timerInterval);
         sendMessage();
@@ -20,5 +96,5 @@ function setTime() {
   
   }
   
-  setTime();
+
   
